3902b256ab7a5497b6f68749b4ab117f80ad7907
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2023-07-17 11:28:06 +0300

    Create crtreq.sh

---
+++ crtreq.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+CMD REQUESTS_CA_BUNDLE=/home/root-ca.crt certbot certonly --manual --preferred-challenges dns -d worker1.nopasaran.com --server https://192.168.123.2:8443/acme/acme/directory --email you@nopasaran.com
