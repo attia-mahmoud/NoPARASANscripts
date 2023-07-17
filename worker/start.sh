0c2e62425135343cfbcc0b1f64dd9d724b954997
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2023-07-17 15:36:20 +0300

    Update start.sh

--- worker/start.sh
+++ worker/start.sh
@@ -3,5 +3,3 @@
 service ssh start
 
 REQUESTS_CA_BUNDLE=/home/root-ca.crt certbot certonly --manual --preferred-challenges dns -d worker1.nopasaran.com --server https://192.168.123.2:8443/acme/acme/directory --email you@nopasaran.com
-
-ssh -R 2222:localhost:22 192.168.123.2
