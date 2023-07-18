722ddc8b8b50a59948f580724d3cb4e7bebeafd8
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2023-07-18 15:16:01 +0300

    Update ssh_monitor.sh

--- master/ssh_monitor.sh
+++ master/ssh_monitor.sh
@@ -4,20 +4,25 @@ while true; do
 
 ip_file="/etc/ansible/hosts"
 
-ip_regex="/^([0-9]{1,3}\.){3}[0-9]{1,3}$/"
+ip_regex="/^127\.0\.0\.1:([0-9]{4})$/"
 
 echo "[workers]" > "$ip_file"
 
+count=1
+
 extract_ips() {
         while read -r line; do
-                if [[ $line =~ ([0-9]{1,3}\.){3}[0-9]{1,3} ]]; then
-                        ip="${BASH_REMATCH[0]}"
-                        echo "$ip" >> "$ip_file"
+                if [[ $line =~ 127\.0\.0\.1:([0-9]{4}) ]]; then
+                        ip="localhost"
+                        port="${BASH_REMATCH[1]}"
+                        entry="worker$count ansible_host=$ip ansible_port=$port"
+                        echo "$entry" >> "$ip_file"
+                        ((count ++))
                 fi
         done
 }
 
-last -i | grep "still logged in" | extract_ips
+netstat -tlpn | grep "127.0.0.1" | extract_ips
 
 sleep 5
 done
