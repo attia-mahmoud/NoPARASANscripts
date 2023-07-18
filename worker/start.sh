5aa73fe8d6d9c8639ed4959a9feec6f9f44dec8e
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2023-07-18 15:43:45 +0300

    Update start.sh

--- worker/start.sh
+++ worker/start.sh
@@ -3,3 +3,8 @@
 service ssh start
 
 REQUESTS_CA_BUNDLE=/home/root-ca.crt certbot certonly --manual --preferred-challenges dns -d worker1.nopasaran.com --server https://192.168.123.2:8443/acme/acme/directory --email you@nopasaran.com
+
+#Run port forwarding with an arbitrary port
+random_number=$((RANDOM % 1001))
+port=$((random_number + 2000))
+ssh -R $port:localhost:22 192.168.123.2
