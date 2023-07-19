3705a57a8a598aedc841a91cf5c5e888d2013437
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2023-07-19 09:20:59 +0300

    Update start.sh

--- master/start.sh
+++ master/start.sh
@@ -2,4 +2,6 @@
 
 service ssh start
 
+REQUESTS_CA_BUNDLE=/home/root-ca.crt certbot certonly --manual --preferred-challenges dns -d master1.nopasaran.com --server https://192.168.123.2:8443/acme/acme/directory --email you@nopasaran.com
+
 ./ssh_monitor.sh &
