268dc0d620c61043546bc284aa76ed612c586ec8
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2023-07-20 17:35:38 +0300

    Update start.sh

--- worker/start.sh
+++ worker/start.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+if [-z "$1" ]; then
+  echo "First argument is missing. Please provide a hostname."
+  exit 1
+fi
+
 service ssh start
 
-REQUESTS_CA_BUNDLE=/home/root-ca.crt certbot certonly --manual --preferred-challenges dns -d worker1.nopasaran.com --server https://192.168.123.2:8443/acme/acme/directory --email you@nopasaran.com
+# REQUESTS_CA_BUNDLE=/home/root-ca.crt certbot certonly --manual --preferred-challenges dns -d worker1.nopasaran.com --server https://192.168.123.2:8443/acme/acme/directory --email you@nopasaran.com
+
+step ssh certificate $1 id_ecdsa
+
+eval "$(ssh-agent -s)"
+ssh-add id_ecdsa
 
 #Run port forwarding with an arbitrary port
 random_number=$((RANDOM % 1001))
